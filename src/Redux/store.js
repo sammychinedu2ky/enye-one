3ed768e96424141693f43f85215fe2e2324f366a
sammychinedu2ky <sammychinedu2ky@gmail.com>
2019-10-23 20:19:14 +0100

    added profiler

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,3 +1,23 @@
-import {createStore} from 'redux'
+
 import rootReducer from './reducer.js'
-export default createStore(rootReducer);
+import { applyMiddleware, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import monitorReducersEnhancer from './enhancers/monitorReducer'
+import loggerMiddleware from './middleware/logger'
+
+
+
+export default function configureStore(preloadedState) {
+  const middlewares = [loggerMiddleware, thunkMiddleware]
+  const middlewareEnhancer = applyMiddleware(...middlewares)
+
+  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
+  const composedEnhancers = composeWithDevTools(...enhancers)
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers)
+
+  return store
+}
+//export default createStore(rootReducer);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom';
 import Demo from './demo';
 import { Provider } from "react-redux";
 import store from "./Redux/store";
-store.subscribe(()=>{
-    console.log(store.getState())
-})
-console.log(store)
-ReactDOM.render(<Provider store={store}><Demo /></Provider>, document.querySelector('#root'));
+
+ReactDOM.render(<Provider store={store()}><Demo /></Provider>, document.querySelector('#root'));
     
